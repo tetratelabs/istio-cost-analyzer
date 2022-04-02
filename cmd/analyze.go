84a152922c7b87bb2d742731ed46b811b244c40b
Aditya Prerepa <adiprerepa@gmail.com>
2022-04-02 12:25:51 -0700

    minified

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -13,6 +13,7 @@ var (
 	cloud       string
 	pricePath   string
 	queryBefore string
+	details     bool
 )
 
 var analyzeCmd = &cobra.Command{
@@ -52,7 +53,7 @@ var analyzeCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		pkg.PrintCostTable(localityCalls, totalCost)
+		pkg.PrintCostTable(localityCalls, totalCost, details)
 		return nil
 	},
 }
@@ -61,5 +62,6 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cloud, "cloud", "gcp", "aws/gcp/azure are provided by default. if nothing is set, gcp is used.")
 	rootCmd.PersistentFlags().StringVar(&pricePath, "pricePath", "", "if custom egress rates are provided, dapani will use the rates in this file.")
 	rootCmd.PersistentFlags().StringVar(&queryBefore, "queryBefore", "0s", "if provided a time duration (go format), dapani will only use data from that much time ago and before.")
+	rootCmd.PersistentFlags().BoolVar(&details, "details", false, "if true, tool will provide a more detailed view of egress costs, including both destination and source")
 	rootCmd.AddCommand(analyzeCmd)
 }
--- pkg/call.go
+++ pkg/call.go
@@ -25,28 +25,76 @@ func (c *Call) StringCost() string {
 	return fmt.Sprintf("%v (%v)->%v (%v) : $%v", c.FromWorkload, c.From.Zone, c.ToWorkload, c.To.Zone, c.CallCost)
 }
 
-// todo minified cost (group by source)
-
-func PrintCostTable(calls []*Call, total float64) {
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Source Workload", "Source Locality", "Destination Workload", "Destination Locality", "Transferred (MB)", "Cost"})
+func PrintCostTable(calls []*Call, total float64, details bool) {
+	// print total
+	totalCost := "<$0.01"
+	if total >= 0.01 {
+		totalCost = fmt.Sprintf("%f", total)
+	}
+	fmt.Printf("\nTotal: %s\n\n", totalCost)
+	if !details {
+		printMinifiedCostTable(calls)
+		return
+	}
+	// sort by cost
 	sort.Slice(calls, func(i, j int) bool {
 		return calls[i].CallCost > calls[j].CallCost
 	})
+	table := tablewriter.NewWriter(os.Stdout)
+	headers := []string{"Source Workload", "Source Locality", "Destination Workload", "Destination Locality", "Transferred (MB)", "Cost"}
+	table.SetHeader(headers)
 	for _, v := range calls {
-		cost := fmt.Sprintf("%f", v.CallCost)
-		if v.CallCost < 0.01 {
-			cost = "<$0.01"
-		}
-		if v.CallCost == 0 {
-			cost = "-"
-		}
-		table.Append([]string{v.FromWorkload, v.From.Zone, v.ToWorkload, v.To.Zone, fmt.Sprintf("%f", float64(v.CallSize)/math.Pow(10, 6)), cost})
+		values := []string{v.FromWorkload, v.From.Zone, v.ToWorkload, v.To.Zone, fmt.Sprintf("%f", float64(v.CallSize)/math.Pow(10, 6)), transformCost(v.CallCost)}
+		table.Append(values)
 	}
 	table.SetBorder(false)
-	table.SetFooter([]string{"", "", "", "", "total", fmt.Sprintf("$%f", total)})
-	fmt.Println()
 	table.Render()
+	fmt.Println()
+}
+
+func printMinifiedCostTable(calls []*Call) {
+	// collapse into set by source
+	// todo should we group by source & locality or just source
+	// 	foo in us-west1-b might be treated diff from foo in us-west1-a
+	//  right now we group by just source, so all localities are grouped in the same row
+	callBySource := make(map[string]Call)
+	for i, v := range calls {
+		if srcCall, ok := callBySource[v.FromWorkload]; !ok {
+			callBySource[v.FromWorkload] = *calls[i]
+		} else {
+			srcCall.CallCost += callBySource[v.FromWorkload].CallCost
+		}
+	}
+	callSlice := make([]Call, 0)
+	for _, v := range callBySource {
+		callSlice = append(callSlice, v)
+	}
+	// order by cost
+	sort.Slice(callSlice, func(i, j int) bool {
+		return callSlice[i].CallCost > callSlice[j].CallCost
+	})
+	// print
+	table := tablewriter.NewWriter(os.Stdout)
+	headers := []string{"Source Workload", "Source Locality", "Cost"}
+	table.SetHeader(headers)
+	for _, v := range callSlice {
+		values := []string{v.FromWorkload, v.From.Zone, transformCost(v.CallCost)}
+		table.Append(values)
+	}
+	table.SetBorder(false)
+	table.Render()
+	fmt.Println()
+}
+
+func transformCost(cost float64) string {
+	costStr := fmt.Sprintf("%f", cost)
+	if cost < 0.01 {
+		costStr = "<$0.01"
+	}
+	if cost == 0 {
+		costStr = "-"
+	}
+	return costStr
 }
 
 type PodCall struct {
